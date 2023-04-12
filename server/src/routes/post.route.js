83a40fc9d0ac8b2c7156cac8576c2942031eb7ad
venkata sudhakar challagulla <ch.v.sudhakar9@gmail.com>
2023-04-12 19:37:41 +0530

    update

--- server/src/routes/post.route.js
+++ server/src/routes/post.route.js
@@ -1,9 +1,7 @@
-const express = require('express');
-const router = express.Router();
+var expres = require('express');
+const router = expres.Router();
 const postController = require('../controllers/post.controller');
 
-
-//router.get('/', userController.getAllUsers); 
 router.get('/posts', postController.getPosts);
 
 module.exports = router;
